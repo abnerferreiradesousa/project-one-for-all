d5ddbb6c8d58eb256ac9c86fc4c718d4142b8cc9
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-04-13 20:22:12 -0300

    [fix] desafio 6.2

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT
-	MIN(P.valor) AS faturamento_minimo,
-    MAX(P.valor) AS faturamento_maximo,
+	ROUND(MIN(P.valor), 2) AS faturamento_minimo,
+    ROUND(MAX(P.valor), 2) AS faturamento_maximo,
     ROUND(AVG(P.valor), 2) AS faturamento_medio,
     ROUND(SUM(P.valor), 2) AS faturamento_total
 FROM SpotifyClone.usuarios AS U
