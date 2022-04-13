8c989a5e4d3e5b495f397208eeaf512b3858ca6b
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-04-13 06:16:19 -0300

    [fix] desafio 3-3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 SELECT
 	U.usuario AS `usuario`,
     COUNT(H.usuario_id) AS qtde_musicas_ouvidas,
-    SUM(M.duracao_segundos) / 60 AS total_minutos
+    ROUND(SUM(M.duracao_segundos) / 60, 2) AS total_minutos
 FROM SpotifyClone.usuarios AS U
 INNER JOIN SpotifyClone.historico AS H
 ON U.usuario_id = H.usuario_id
