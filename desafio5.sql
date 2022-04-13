c62b687c7d9599c6c08d5f83ebdec3f378741b7d
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-04-13 12:09:15 -0300

    [feat] desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT M.title AS cancao, COUNT(M.musica_id) AS reproducoes
+FROM SpotifyClone.historico AS H
+INNER JOIN SpotifyClone.musicas AS M
+ON H.musica_id = M.musica_id
+GROUP BY H.musica_id
+ORDER BY reproducoes DESC, M.title
+LIMIT 2;
